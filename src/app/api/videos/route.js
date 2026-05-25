848610f138f90f073d505c863e3494b6cf6cbcc3
Nesse-org <jy288794@gmail.com>
2026-05-25 17:53:34 +0500

    fix: replaced restricted videos with 100 percent embeddable public clips

--- src/app/api/videos/route.js
+++ src/app/api/videos/route.js
@@ -1,22 +1,22 @@
 import { NextResponse } from 'next/server';
 
-const mrBeastVideos = [
-  { id: "1", title: "I Survived 100 Days In An Absolute Wilderness", views: "142M views", time: "2 months ago", duration: "24:15", videoId: "iogcY_4xGjo" },
-  { id: "2", title: "I Opened A Restaurant That Pays You To Eat", views: "180M views", time: "5 months ago", duration: "13:31", videoId: "Dg86p9BdfqE" },
-  { id: "3", title: "I Survived 7 Days In Solitary Confinement", views: "190M views", time: "8 months ago", duration: "20:05", videoId: "gHzuHo90wKM" },
-  { id: "4", title: "Would You Press This Button For $1,000,000?", views: "215M views", time: "1 year ago", duration: "14:52", videoId: "fmfR-eJmrm8" },
-  { id: "5", title: "Ages 1 - 100 Fight For $500,000", views: "340M views", time: "1 year ago", duration: "28:11", videoId: "2isYuQZMbdU" },
-  { id: "6", title: "I Bought Everything In A Store", views: "260M views", time: "6 months ago", duration: "14:12", videoId: "Rj_vssRaZlQ" },
-  { id: "7", title: "1,000 Blind People See For The First Time", views: "185M views", time: "1 year ago", duration: "14:15", videoId: "TJ2ifmkGGus" },
-  { id: "8", title: "I Spent 24 Hours In A House Of Cards", views: "195M views", time: "9 months ago", duration: "12:50", videoId: "S87J89fS_H0" }
+const openVideos = [
+  { id: "1", title: "Official Blender Cinematic Animation - Spring", views: "55M views", time: "2 years ago", duration: "07:44", videoId: "CkWqChHaNMw" },
+  { id: "2", title: "Sintel - Open Source HD Movie Project", views: "89M views", time: "3 years ago", duration: "14:48", videoId: "eRsGyueVLvQ" },
+  { id: "3", title: "Big Buck Bunny - 4K Ultra HD Classic", views: "45M views", time: "5 years ago", duration: "09:56", videoId: "aqz-KE-bpKQ" },
+  { id: "4", title: "Tears of Steel - Sci-Fi VFX Showcase", views: "34M views", time: "4 years ago", duration: "12:14", videoId: "R6MlUcmO1A4" },
+  { id: "5", title: "Cosmos Laundromat - Award Winning Short Animation", views: "12M views", time: "1 year ago", duration: "12:10", videoId: "Y-rmzh0PII8" },
+  { id: "6", title: "Caminandes 3: Llamigos - Funny Cartoon Loop", views: "67M views", time: "8 months ago", duration: "02:30", videoId: "SkVqJ1SGeL0" },
+  { id: "7", title: "Agent 327: Operation Barbershop Action Trailer", views: "15M views", time: "1 year ago", duration: "03:51", videoId: "mN0zPOpADL4" },
+  { id: "8", title: "Glass Half - Full 3D Stylized Short Film", views: "21M views", time: "6 months ago", duration: "03:00", videoId: "Yd7788vscvE" }
 ];
 
-const mrBeastShorts = [
-  { id: "s1", title: "Giving Away $10,000 If You Can Do This!", likes: "12M", videoId: "sh98mE34Yq0" },
-  { id: "s2", title: "Testing World's Craziest Gadgets!", likes: "8.5M", videoId: "pT79xV_Ssh8" },
-  { id: "s3", title: "He Passed The Hardest Challenge!", likes: "15M", videoId: "vB-B_f0vR5g" },
-  { id: "s4", title: "World's Safest Safe vs Military Explosives!", likes: "9.1M", videoId: "fPp39fubP3E" },
-  { id: "s5", title: "Will The Hydraulic Press Destroy This?", likes: "14M", videoId: "ZpEon6VCH-g" }
+const openShorts = [
+  { id: "s1", title: "Nature Relaxation 4K Loop - Beautiful River", likes: "5M", videoId: "9xuR73_LpFM" },
+  { id: "s2", title: "Satisfying Hydraulic Press vs Metal Objects", likes: "8M", videoId: "O91DRZ1CAs8" },
+  { id: "s3", title: "Tokyo City Street Hyperlapse view", likes: "3M", videoId: "X8zLJlUf6wU" },
+  { id: "s4", title: "Calm Ocean Waves and Sand Drone Shot", likes: "12M", videoId: "W0LHTWG-UmQ" },
+  { id: "s5", title: "Synthwave Neon Grid Landscape Animation", likes: "7M", videoId: "4S1b9vSh0G0" }
 ];
 
 export async function GET(req) {
@@ -24,7 +24,7 @@ export async function GET(req) {
   const type = searchParams.get('type');
   
   if (type === 'shorts') {
-    return NextResponse.json(mrBeastShorts);
+    return NextResponse.json(openShorts);
   }
-  return NextResponse.json(mrBeastVideos);
+  return NextResponse.json(openVideos);
 }
